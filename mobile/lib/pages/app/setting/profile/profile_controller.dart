b24817670d1dd08cfff91b410defddee25e53c72
hieunm <hieunm@zyyx.jp>
2025-04-21 16:40:38 +0700

    ui: update image ui

--- mobile/lib/pages/app/setting/profile/profile_controller.dart
+++ mobile/lib/pages/app/setting/profile/profile_controller.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:mobile/config/env.dart';
 import 'package:mobile/pages/app/app_controller.dart';
 import 'package:mobile/theme/theme_controller.dart';
@@ -11,6 +14,10 @@ class ProfileController extends GetxController {
   final ThemeController themeController = Get.find<ThemeController>();
   final AppController appController = Get.find<AppController>();
   final serverHost = Env.serverhost;
+
+  final ImagePicker imagePicker = ImagePicker();
+  Rx<File?> image = Rx<File?>(null);
+
 
   final name = ''.obs;
   final email = ''.obs;
@@ -34,8 +41,87 @@ class ProfileController extends GetxController {
     newEmail.value = prefs.getString('email') ?? '';
   }
 
-  Future<void> updateInfo() async {
+  Future<void> pickImage(context) async {
+    print('pickImage');
+    final pickedFile = await imagePicker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      image.value = File(pickedFile.path);
+    }
     
+    showModalBottomSheet(
+      context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.zero,
+      ),
+      builder: (context) => Container(
+        color: themeController.isDark.value ? Colors.transparent : Colors.white,
+        padding: EdgeInsets.all(20),
+        width: double.infinity,
+        height: Get.width + 50,
+        child: Column(
+          children: [
+            Text(
+              'Preview',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 15),
+            
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 1,
+                ),
+                shape: BoxShape.circle,
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(120),
+                child: Image.file(
+                  image.value!,
+                  width: 240,
+                  height: 240,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            GestureDetector(
+              onTap: () {
+                updateInfo();
+              },
+              child: Container(
+                width: double.infinity,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Text(
+                    'Update',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> updateInfo() async {
+    print('updateInfo');
   }
 
 }
--- mobile/lib/pages/app/setting/profile/profile_view.dart
+++ mobile/lib/pages/app/setting/profile/profile_view.dart
@@ -22,8 +22,48 @@ class ProfileView extends GetView<ProfileController> {
                   child: Stack(
                     children: [
                       GestureDetector(
-                        onTap: () => {
-                          print(1)
+                        onTap: () {
+                          showModalBottomSheet(
+                            context: context,
+                            isScrollControlled: true,
+                            builder: (BuildContext context) {
+                              return Stack(
+                                children: [
+                                  Container(
+                                    color: Colors.black,
+                                    child: Center(
+                                      child: Image.network(
+                                        controller.supabaseUrl + controller.appController.imageUrl.value,
+                                        width: MediaQuery.of(context).size.width,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    top: 40,
+                                    right: 20,
+                                    child: GestureDetector(
+                                      onTap: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: Container(
+                                        padding: const EdgeInsets.all(8),
+                                        decoration: BoxDecoration(
+                                          color: Colors.black,
+                                          shape: BoxShape.circle,
+                                        ),
+                                        child: Icon(
+                                          Icons.close,
+                                          color: Colors.white,
+                                          size: 24,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
                         },
                         child: Container(
                           decoration: BoxDecoration(
@@ -42,24 +82,24 @@ class ProfileView extends GetView<ProfileController> {
                               fit: BoxFit.cover,
                             ),
                           ),
-                        ),                 
+                        ),
                       ),
                       Positioned(
                         bottom: 3,
                         right: 3,
                         child: GestureDetector(
                           onTap: () {
-                            print(2);
+                            controller.pickImage(context);
                           },
                           child: Container(
                             padding: const EdgeInsets.all(5),
                             decoration: BoxDecoration(
-                              color: Colors.black.withAlpha(150),
+                              color: controller.themeController.isDark.value ? Colors.white.withAlpha(150) : Colors.black.withAlpha(150),
                               shape: BoxShape.circle,
                             ),
                             child: Icon(
                               BoxIcons.bxs_pencil,
-                              color: Colors.white,
+                              color: controller.themeController.isDark.value ? Colors.black : Colors.white,
                               size: 24
                             ),
                           ),
@@ -71,32 +111,47 @@ class ProfileView extends GetView<ProfileController> {
                   
                 const SizedBox(height: 20),
                 
-                Container(
-                  child: TextFormField(
-                    onChanged: (value) {
-                      controller.name.value = value;
-                    },
-                    initialValue: controller.appController.name.value,
-                    decoration: InputDecoration(
-                      labelText: 'Name',
-                      border: OutlineInputBorder(),
+                TextFormField(
+                  onChanged: (value) {
+                    controller.name.value = value;
+                  },
+                  initialValue: controller.appController.name.value,
+                  cursorColor: Colors.grey,
+                  decoration: InputDecoration(
+                    labelText: 'Name',
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    labelStyle: TextStyle(color: Colors.grey),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(15),
                     ),
                   ),
                 ),
                   
                 const SizedBox(height: 20),
-
-                Container(
-                  child: TextFormField(
-                    onChanged: (value) {
-                      controller.email.value = value;
-                    },
-                    initialValue: controller.appController.email.value,
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      border: OutlineInputBorder(),
+                
+                TextFormField(
+                  onChanged: (value) {
+                    controller.email.value = value;
+                  },
+                  initialValue: controller.appController.email.value,
+                  cursorColor: Colors.grey,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: TextStyle(color: Colors.grey),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(15),
                     ),
                   ),
+                  
                 ),
                   
                 const SizedBox(height: 20),
